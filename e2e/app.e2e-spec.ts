5846bf3d2f51dd460bdd91ca23f2906dc58e4007
CesarSalazar25 <cesarsalazarlopez25@gmail.com>
2018-04-01 18:40:00 -0400

    Recovery

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('aechmea-proy-no-sql App', () => {
+describe('proyecto-ii-aechmea App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: true,
+
+  //Se conecta con la base de datos Realtime hecha en Firebase:
+  firebase: {
+    apiKey: "AIzaSyDbfn_hdvvMu46afLwJrF6SUmBJK4YZexg",
+    authDomain: "aechmea-flora-y-fauna-de-vzla.firebaseapp.com",
+    databaseURL: "https://aechmea-flora-y-fauna-de-vzla.firebaseio.com",
+    projectId: "aechmea-flora-y-fauna-de-vzla",
+    storageBucket: "aechmea-flora-y-fauna-de-vzla.appspot.com",
+    messagingSenderId: "491749486362"
+  } 
 };
