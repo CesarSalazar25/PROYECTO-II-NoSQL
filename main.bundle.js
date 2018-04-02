9c2492faa5542f91d1a9c096e85b5465897f69e4
CesarSalazar25 <cesarsalazarlopez25@gmail.com>
2018-04-01 22:20:21 -0400

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -821,8 +821,12 @@ var FotosService = /** @class */ (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: true,
+    production: false,
     //Se conecta con la base de datos Realtime hecha en Firebase:
     firebase: {
         apiKey: "AIzaSyDbfn_hdvvMu46afLwJrF6SUmBJK4YZexg",
