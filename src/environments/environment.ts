3481bd58fb44c37889e7bfd42ad173552b9878ab
CesarSalazar25 <cesarsalazarlopez25@gmail.com>
2018-04-01 22:12:45 -0400

    Prueba Github Pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false,
 
   //Se conecta con la base de datos Realtime hecha en Firebase:
   firebase: {
