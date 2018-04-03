18dbde25ab14d2c1064de76e505ccfb40eb6af53
CesarSalazar25 <35537702+CesarSalazar25@users.noreply.github.com>
2018-04-03 03:53:27 -0400

    acomodando paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { FaunaComponent }   from './components/datos/fauna/fauna.component';
 import { FloraComponent }      from './components/datos/flora/flora.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/datos', pathMatch: 'full' },
-  { path: 'fauna', component:  FaunaComponent },
-  { path: 'flora', component: FloraComponent },
+  { path: '', redirectTo: ' https://cesarsalazar25.github.io/datos', pathMatch: 'full' },
+  { path: ' https://cesarsalazar25.github.io/fauna', component:  FaunaComponent },
+  { path: ' https://cesarsalazar25.github.io/flora', component: FloraComponent },
 ];
 
 @NgModule({
